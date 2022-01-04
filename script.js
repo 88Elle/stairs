7323b26c0ce67e33f066d3b9e7776523349c64b5
elle <elle@bk.ru>
2022-01-04 23:41:06 +0300

    save

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+ let a = +prompt('Введите число');
+ let b = prompt('Введите символ отступа');
+ let c = prompt('Введите конечный символ');
+
+ for(let i = 0; i < a; i++) {
+    if (i == 0) {
+        c = c
+   }else{
+       c = b + c
+   }
+   console.log(c);
+   }
+
+
+
+  
